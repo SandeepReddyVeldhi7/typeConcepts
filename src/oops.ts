0618c5e024b9027ba40bda675caa17def29e8e10
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2026-01-09 23:07:31 +0530

    add initial implementation of Sandeep class with constructor and logging

---
+++ src/oops.ts
@@ -0,0 +1,11 @@
+class Sandeep{
+    name:string;
+    age:number;
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age;
+    }
+}
+
+const sandeeps=new Sandeep("sandeep",22)
+console.log(sandeeps)
